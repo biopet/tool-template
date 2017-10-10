ed59f4e96c61347121889fd7bf99f758f524c88e
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-10 21:22:25 +0200

    Added template to all tools

--- src/test/scala/nl/biopet/tools/template/ToolTemplateTest.scala
+++ src/test/scala/nl/biopet/tools/template/ToolTemplateTest.scala
@@ -5,11 +5,6 @@ import org.testng.annotations.Test
 
 class ToolTemplateTest extends BiopetTest {
   @Test
-  def testMain(): Unit = {
-    ToolTemplate.main(Array("-i", "inputFile"))
-  }
-
-  @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
       ToolTemplate.main(Array())
